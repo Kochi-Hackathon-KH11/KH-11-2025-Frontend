418d1a84cc90328c957db3f651b50d377f194560
71203mayank <mayank71203@gmail.com>
2025-02-15 20:10:38 +0530

    initialized home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import AudioPlayer from "@/components/AudioPlayer";
+import HomeSection from "@/components/home/home";
 import RecordLayout from "@/components/RecordLayout"
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <>
+    {/* <div className="flex justify-center items-center h-screen">
       <RecordLayout></RecordLayout>
-    </div>
+    </div> */}
+    <HomeSection/>
+    </>
   );
 }
