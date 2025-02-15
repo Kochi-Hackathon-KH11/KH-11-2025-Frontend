6546db1c2bef2fcc56ce7e37239be575b2159802
th3bossc <diljith2003@gmail.com>
2025-02-16 02:11:28 +0530

    made changes

--- src/lib/signaling.ts
+++ src/lib/signaling.ts
@@ -1,24 +1,22 @@
 import io, { Socket } from 'socket.io-client';
 
-const SIGNALING_SERVER_URL = '';
-// signaling server URL here
+const SIGNALING_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
-export class Signaling{
+export class Signaling {
     private socket: Socket;
-    private onOffer : (data: any) => void;
+    private onOffer: (data: any) => void;
     private onAnswer: (data: any) => void;
     private onCandidate: (data: any) => void;
     private onAuthenticated: () => void;
     private onAuthError: (message: string) => void;
     private onUsersUpdated: (users: any[]) => void;
 
-    constructor(){
+    constructor() {
         this.socket = io(SIGNALING_SERVER_URL, {
-            transports:['websocket'],
+            transports: ['websocket'],
             autoConnect: false,
         });
 
-        // Event Listeners
         this.socket.on('connect', this.handleConnect);
         this.socket.on('disconnect', this.handleDisconnect);
         this.socket.on('authenticated', this.handleAuthenticated);
@@ -29,33 +27,33 @@ export class Signaling{
     }
 
     // connect to the signaling server
-    connect(): void{
+    connect(): void {
         this.socket.connect();
     }
 
     // Disconnect from the signaling server
-    disconnect(): void{
+    disconnect(): void {
         this.socket.disconnect();
     }
 
     // Authenticate the user
     authenticate(username: string, password: string): void {
-        this.socket.emit('authenticate_user', {username, password});
+        this.socket.emit('authenticate_user', { username, password });
     }
 
     // send WebRTC offer
     sendOffer(to: string, offer: any): void {
-        this.socket.emit('offer', {to, offer});
+        this.socket.emit('offer', { to, offer });
     }
 
     // send WebRTC answer
-    sendAnswer(to:string, answer: any): void {
-        this.socket.emit('answer', {to, answer});
+    sendAnswer(to: string, answer: any): void {
+        this.socket.emit('answer', { to, answer });
     }
 
     // Send ICE candidate
     sendCandidate(to: string, candidate: any): void {
-        this.socket.emit('candidate', {to, candidate});
+        this.socket.emit('candidate', { to, candidate });
     }
 
     // Fetch all users
@@ -71,41 +69,35 @@ export class Signaling{
     private handleDisconnect = () => {
         console.log('Disconnected from signaling server');
     };
-    
+
     private handleAuthenticated = () => {
-        if (this.onAuthenticated) {
-        this.onAuthenticated();
-        }
+        if (this.onAuthenticated)
+            this.onAuthenticated();
     };
-    
+
     private handleAuthError = (data: { message: string }) => {
-        if (this.onAuthError) {
-        this.onAuthError(data.message);
-        }
+        if (this.onAuthError)
+            this.onAuthError(data.message);
     };
-    
+
     private handleOffer = (data: any) => {
-        if (this.onOffer) {
-        this.onOffer(data);
-        }
+        if (this.onOffer)
+            this.onOffer(data);
     };
 
     private handleAnswer = (data: any) => {
-        if (this.onAnswer) {
-        this.onAnswer(data);
-        }
+        if (this.onAnswer)
+            this.onAnswer(data);
     };
-    
+
     private handleCandidate = (data: any) => {
-        if (this.onCandidate) {
-        this.onCandidate(data);
-        }
+        if (this.onCandidate)
+            this.onCandidate(data);
     };
-    
+
     private handleUsersUpdated = (users: any[]) => {
-        if (this.onUsersUpdated) {
-        this.onUsersUpdated(users);
-        }
+        if (this.onUsersUpdated)
+            this.onUsersUpdated(users);
     };
 
     // Set event callbacks
